51818367726e3b3930ce4841e60fc1ca29cbcf97
mustard221 <C24432034@mytudublin.ie>
2024-10-07 16:47:34 +0100

    week4

---
+++ the-course/shooter game.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+@export var bullet_scene:PackedScene
+@export var bullet_spawn:Node2D
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -400.0
+
+const TURN_RATE = 180
+
+func _physics_process(delta: float) -> void:
+	
+	var r = Input.get_axis("turn_left", "turn_right")
+
+	var rot = deg_to_rad(r * TURN_RATE * delta)
+	rotate(rot)
+	
+	var f = Input.get_axis("move_backwards", "move_forwards")
+
+	var vel = transform.y * f * SPEED
+	velocity = vel
+	
+	if Input.is_action_pressed("fire"):
+		var b = bullet_scene.instantiate()
+		b.global_position = bullet_spawn.global_position
+		b.global_rotation = bullet_spawn.global_rotation
+		get_tree().get_root().add_child(b) 
+	
+	move_and_slide()
