4f85c7490dfeee3c52a44068512a60caa967b943
mustard221 <C24432034@mytudublin.ie>
2024-11-11 22:02:44 +0000

    merge

--- the-course/enemy.gd
+++ the-course/enemy.gd
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-extends CharacterBody2D
-
-var SPEED = 200.0
-
-func move_forwards():
-	velocity.x = -200
-func move_backwards():
-	velocity.x = 200
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
-
-func _physics_process(delta):
-	move_forwards()
-	move_backwards()
-	move_and_slide()
-=======
 extends Sprite2D
 
 @export var player:Node2D
@@ -37,4 +19,3 @@ func _process(delta: float) -> void:
 	global_position = global_position + (- to_player * speed * delta)
 	
 	pass
->>>>>>> 6878b50158a291b71d9fdde59babafc75d9bae0f
--- the-course/player.gd
+++ the-course/player.gd
@@ -8,25 +8,6 @@ const JUMP_VELOCITY = -400.0
 
 const TURN_RATE = 180
 
-<<<<<<< HEAD
-func _print_stuff():
-	DebugDraw2D.set_text("pos", position)
-	DebugDraw2D.set_text("rotation", rotation)
-	DebugDraw2D.set_text("rotation deg", rad_to_deg(rotation))
-	DebugDraw2D.set_text("transform.x", transform.x)
-	DebugDraw2D.set_text("transform.y", transform.y)
-	DebugDraw2D.set_text("global_transform.x", global_transform.x)
-	DebugDraw2D.set_text("global_transform.y", global_transform.y)
-	DebugDraw2D.set_text("vel", velocity)
-
-func _process(delta: float) -> void:
-	_print_stuff()
-
-func _physics_process(delta: float) -> void:
-	
-	var r = Input.get_axis("turn _left", "turn_right")
-	
-=======
 func print_stuff():
 	DebugDraw2D.set_text("pos", position)
 	DebugDraw2D.set_text("global position", global_position)
@@ -49,18 +30,12 @@ func _physics_process(delta: float) -> void:
 	var r = Input.get_axis("turn_left", "turn_right")
 	print(r)
 
->>>>>>> 6878b50158a291b71d9fdde59babafc75d9bae0f
 	var rot = deg_to_rad(r * TURN_RATE * delta)
 	
 	var f = Input.get_axis("move_backwards", "move_forwards")
-<<<<<<< HEAD
-	
-	var vel = transform.x * f * SPEED
-=======
 
 	var vel = - transform.y * f * SPEED
 	print(transform.y)
->>>>>>> 6878b50158a291b71d9fdde59babafc75d9bae0f
 	velocity = vel
 	
 	
