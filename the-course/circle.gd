828ecee234f73f2ed625f16d1c6934c062130e8b
mustard221 <C24432034@mytudublin.ie>
2024-11-18 15:58:42 +0000

    if_statements

---
+++ the-course/circle.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _draw():
+	
+	draw_circle(Vector2(575, 255), 9.3905, Color.WHITE)
+	
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -1,10 +1,35 @@
 extends Node2D
 
-func _draw() -> void:
-	var r = get_viewport_rect()
-	r.size.x = r.size.x / 2
-	draw_rect(Rect2(r), Color.RED, true, 10, true)
+var i = 0
 
+func _draw() -> void:
+	i = 1
+	var r = get_viewport_rect()
+	var h = r.size.x / 2
+	var l = r.size.y / 2
+	
+	draw_rect(Rect2(0, 0, h, l), Color.RED, true)
+	draw_rect(Rect2(h, 0, h, l), Color.BLUE, true)
+	draw_rect(Rect2(0, l, h, h), Color.YELLOW, true)
+	draw_rect(Rect2(0, l, h, 0), Color.GREEN, true)
+	
+	var p = get_viewport().get_mouse_position()
+	print(p)
+	
+	if p.x < h and p.y < l:
+		draw_rect(Rect2(0, 0, h, l), Color.RED, true)
+	
+	elif p.x > h and p.y < l:
+		draw_rect(Rect2(h, 0, h, l), Color.YELLOW, true)
+	
+	elif p.x < h and p.y > l:
+		draw_rect(Rect2(0, l, h, h), Color.GREEN, true)
+
+	elif p.x > h and p.y > l:
+		draw_rect(Rect2(0, l, h, 0), Color.BLUE, true)
+	
+	pass
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +37,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	queue_redraw()
 	pass
