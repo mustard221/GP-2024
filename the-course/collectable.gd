a58a1f39141c70147bb0a935af0ec95860acca8f
mustard221 <C24432034@mytudublin.ie>
2025-02-27 13:20:55 +0000

    lab exercises

--- the-course/collectable.gd
+++ the-course/collectable.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var collected = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,5 +15,7 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "platformer_dude":
 		body.score = body.score + 1
+		collected = body.score
+		$"../platformer_dude/Camera2D/Label".text = "Collected: " + str(collected)
 		self.queue_free()
 	pass # Replace with function body.
