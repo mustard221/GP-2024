758076c42a03c0671d7b496e8c6d940770cf3f7e
mustard221 <C24432034@mytudublin.ie>
2025-03-27 13:15:29 +0000

    3d_stuff

--- the-course/gun_dude.gd
+++ the-course/gun_dude.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_pressed("fire"):
 		var b = bullet_scene.instantiate()
 		$"../../../".add_child(b)
 
